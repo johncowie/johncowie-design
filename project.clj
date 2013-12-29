0c71a55dc1db3602d9a289f34c0304e17adb5315
John Cowie <john.a.cowie@gmail.com>
2013-12-29 16:31:32 +0000

    First commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject johncowie-design "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]
+                 [garden "1.1.4"]]
+  :plugins [[lein-ring "0.8.8"]]
+  :ring {:handler johncowie-design.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
---
+++ src/johncowie_design/handler.clj
@@ -0,0 +1,125 @@
+(ns johncowie-design.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [hiccup.page :refer [include-js include-css html5]]
+            [hiccup.core :refer [html]]
+            [garden.core :refer [css]]
+            [garden.units :refer [px pc]]
+            [garden.stylesheet :refer [at-media]]
+            [ring.util.response :refer [response content-type]]))
+
+(def width (px 600))
+
+(defn make-palette [header-bg header-text post-bg post-text links]
+  {:header-bg header-bg
+   :header-text header-text
+   :post-bg post-bg
+   :post-text post-text
+   :links links}
+  )
+
+(def palettes {
+:a (make-palette :#BC294E :#26103D :#C7C1C2 :#3D2B5C :#912049)
+:b (make-palette :#036564 :#E8DDCB :#E8DDCB :#033649 :#031634)
+:c (make-palette :#555555 :#ffffff :#ffffff :#000000 :#ff0000)
+})
+
+(def palette (:c palettes))
+
+(defn generate-css []
+(css
+ [:body
+    {:font-family ["\"Open Sans\"" "sans-serif"]
+     :font-size (px 18)
+     :background-color (:post-bg palette)
+     :color (:post-text palette)
+     :padding (px 0)
+     :margin (px 0)}
+  ]
+ [:a
+    {:color (:links palette)
+     :text-decoration :none}
+  ]
+ [:code
+    {:display :block
+     :background-color :white
+     :margin [[(px 10) (px 0)]]
+     :padding (px 10)}
+  ]
+ [:.nav
+    {
+     :display :block
+     :width "100%"
+     :background-color (:post-text palette)
+     }
+    [:ul {:margin 0 :padding 0}]
+    [:li {:display :inline-block
+          :margin (px 5)}]
+    [:a  {:color (:header-text palette)}
+       [:&.selected
+         {:color :#ff0000}
+        ]
+     ]
+  ]
+ [:.container {:display :block
+               :margin [[(px 0) :auto (px 0) :auto]]
+               :width width}
+   (at-media {:max-width width}
+           [:& {:width "100%"}])]
+
+ [:.header
+    {:display :block
+     :background-color (:header-bg palette)
+     :color (:header-text palette)
+     :margin (px 0)
+     :padding (px 5)
+     :text-align :center
+     }
+    (at-media {:max-width width}
+              [:& {:font-size (px 50)}])
+  ]
+ [:.content
+    {:padding (px 10)
+     :line-height 1.7}
+  ]
+ [:.footer
+    {:padding (px 10)}
+    [:.next {:float :right}]
+    [:.prev {:float :left}]
+  ]
+
+ [:.clear-fix {:clear :both}]))
+
+(defn base-template []
+   (html5
+   [:head
+     [:title "Test Design"]
+     ;(include-css "http://fonts.googleapis.com/css?family=Open+Sans:300italic,300,400italic,400,600italic,600,700italic,700,800italic,800")
+     ;(include-css "js/google-code-prettify/prettify.css")
+     (include-css "/css")
+    ]
+   [:body
+    [:div.header
+       [:span.container "JOHN COWIE"]]
+    [:div.nav
+       [:div.container
+         [:ul
+           [:li [:a.blog-link {:href "http://blog.johncowie.co.uk"} "Blog"]]
+           [:li [:a.reviews-link {:href "http://reviews.johncowie.co.uk"} "Reviews"]]
+          ]
+        ]
+     ]
+    [:div.container.content
+      [:h1 "Test Header"]
+      [:p "This is some test content.  Blahdy blahdy blah."]
+     ]]))
+
+(defroutes app-routes
+  (GET "/" [] (base-template))
+  (GET "/css" [] (content-type (response (generate-css)) "text/css"))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
